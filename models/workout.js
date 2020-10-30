66680ee645bd9ffa09cdd3fc38392c0dce7d4a53
Ridge  Khan <ridgekhan@Ridges-MBP.attlocal.net>
2020-10-29 20:31:54 -0500

    routes, models, servers

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "Type of fitness",
+                trim: true
+            },
+            name: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            duration: {
+                type: Number,
+                required: "Duration in minutes",
+                trim: true
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
+
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+// path for relative routes to our HTML files
+const path = require("path");
+const router = require("express").Router()
+
+
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
